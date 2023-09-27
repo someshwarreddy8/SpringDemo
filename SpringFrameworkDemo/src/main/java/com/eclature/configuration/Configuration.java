7ead9f4deb666d4160568fccd44356111f43fba7
someshwarreddydasari <someshwar.dasari@eclature.com>
2023-09-27 11:46:42 +0530

    first commit

--- SpringFrameworkDemo/src/main/java/com/eclature/configuration/Configuration.java
+++ SpringFrameworkDemo/src/main/java/com/eclature/configuration/Configuration.java
@@ -5,5 +5,5 @@ import org.springframework.context.annotation.ComponentScan;
 @org.springframework.context.annotation.Configuration
 @ComponentScan("com.eclature")
 public class Configuration {
-
+ 
 }
